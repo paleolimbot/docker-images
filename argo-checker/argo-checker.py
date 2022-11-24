6bee791e732b89269c8b2ad030f8fd8d2ac36e38
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 03:52:35 +0000

    Adding tarfile member sanitization to extractall()

--- argo-checker/argo-checker.py
+++ argo-checker/argo-checker.py
@@ -55,7 +55,26 @@ def install_tool_from_url(url, dest):
 
         inform_status('Extracting...')
         with tarfile.open(tmp) as tar:
-            tar.extractall(install_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, install_root)
     finally:
         os.close(fd)
         os.unlink(tmp)
